b1b08933ae1f13f4c00b179612dabb077100ecb3
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-11 17:05:21 -0300

    feat: adicionando o typescript para renderizar os estados via ngOnInit e habilitar selecao das cidades por estado

--- questao-9/questao-9-lifecycle-hooks/src/app/main-container/main-container.component.ts
+++ questao-9/questao-9-lifecycle-hooks/src/app/main-container/main-container.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main-container',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './main-container.component.html',
   styleUrl: './main-container.component.css'
 })
-export class MainContainerComponent {
+export class MainContainerComponent implements OnInit {
+  email: string = '';
+  nome: string = '';
+  estado: string = '';
+  cidade: string = '';
+  instituicao: string = '';
 
+  estados: string[] = [];
+  cidadesDisponiveis: string[] = [];
+
+
+  cidadesPorEstado: { [estado: string]: string[] } = {
+    'SP': ['São Paulo', 'Campinas', 'Santos'],
+    'RJ': ['Rio de Janeiro', 'Niterói', 'Petrópolis'],
+    'PE': ['Recife', 'Vitória de Santo Antão', 'Olinda']
+  };
+
+  ngOnInit(): void {
+    // preenche os estados com base no objeto de cidades ao INICIAR A APLICACAO
+    this.estados = Object.keys(this.cidadesPorEstado);
+  }
+
+  atualizarCidades(): void {
+    this.cidadesDisponiveis = this.cidadesPorEstado[this.estado] || [];
+    this.cidade = ''; // limpa a cidade ao trocar de estado
+  }
 }
