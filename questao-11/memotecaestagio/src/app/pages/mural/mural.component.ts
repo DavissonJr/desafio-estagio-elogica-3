1118f87ed1918c2a34230ad06fa2a00dc0320510
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-14 14:33:36 -0300

    feat: criar componentes e services base

---
+++ questao-11/memotecaestagio/src/app/pages/mural/mural.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mural',
+  imports: [],
+  templateUrl: './mural.component.html',
+  styleUrl: './mural.component.css'
+})
+export class MuralComponent {
+
+}
