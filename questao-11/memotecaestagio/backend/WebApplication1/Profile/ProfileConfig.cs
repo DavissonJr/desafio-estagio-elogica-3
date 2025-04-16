4f9632076302292c12742d78baa7231c06e82f46
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-16 14:06:21 -0300

    build: configurar o automapper

---
+++ questao-11/memotecaestagio/backend/WebApplication1/Profile/ProfileConfig.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using WebApplication1.Domain;
+using WebApplication1.Dto;
+
+public class ConfigProfile : Profile
+{
+    public ConfigProfile()
+    {
+        CreateMap<CreateAutorDto, Autor>().ReverseMap();
+    }
+}
