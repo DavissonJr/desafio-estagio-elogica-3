d33251cb5c9d8204be9cc67d3d982b197a205922
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-16 13:27:21 -0300

    feat: mudança no codigo para ativar a reatividade

--- questao-11/memotecaestagio/src/app/shared/components/cards/cards.component.ts
+++ questao-11/memotecaestagio/src/app/shared/components/cards/cards.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cards',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.css'
 })
 export class CardsComponent {
   @Input() modo: 'mural' | 'formulario' = 'mural';
+  @Input() pensamento: string = '';
+  @Input() autor: string = '';
+  @Input() modelo: string = 'modelo1';
 }
