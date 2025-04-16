ff6d5658fbf04e73258ecddb159e189fbde7de09
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-16 15:56:54 -0300

    fix: ajustar as cores ao redor do input no fomulario

--- questao-11/memotecaestagio/frontend/src/app/shared/components/cards/cards.component.ts
+++ questao-11/memotecaestagio/frontend/src/app/shared/components/cards/cards.component.ts
@@ -2,30 +2,20 @@ import { CommonModule } from "@angular/common";
 import { Component, Input } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
-// REMOVA o @Input da sombra
 @Component({
   selector: 'app-cards',
   standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './cards.component.html',
-  styleUrl: './cards.component.css'
+  styleUrls: ['./cards.component.css']
 })
 export class CardsComponent {
   @Input() modo: 'mural' | 'formulario' = 'mural';
   @Input() pensamento: string = '';
   @Input() autor: string = '';
   @Input() modelo: string = 'modelo1';
+  @Input() sombra: string = '';
 
-  get sombra(): string {
-    switch (this.modelo) {
-      case 'modelo1':
-        return 'shadow-blue-dark';
-      case 'modelo2':
-        return 'shadow-blue-light';
-      case 'modelo3':
-        return 'shadow-green';
-      default:
-        return '';
-    }
-  }
+  get sombraClasse() { return this.sombra || ''; }
 }
+
