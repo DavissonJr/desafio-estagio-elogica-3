cff24da6b7fb0e7e786a36888ce773f592baa831
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-16 14:06:01 -0300

    feat: criar service e sua interace

---
+++ questao-11/memotecaestagio/backend/WebApplication1/Services/Interfaces/IAutorService.cs
@@ -0,0 +1,12 @@
+﻿using WebApplication1.Domain;
+
+namespace WebApplication1.Services.Interfaces;
+
+public interface IAutorService
+{
+    Task<Autor> BuscaAutorPorId(int id);
+    Task<List<Autor>> BuscarTodosAutores();
+    Task<bool> InserirAutor(Autor autor);
+    Task<bool> AtualizarAutores(Autor autor);
+    Task<bool> DeletarAutor(int id);
+}
