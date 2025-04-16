c118a4b7acdcc636499e093c9dfa7dbd73ba88a8
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-16 14:04:50 -0300

    feat: estuturar o program com as injecoes de dependencia

---
+++ questao-11/memotecaestagio/backend/WebApplication1/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Text.Json;
+using WebApplication1.Infra.Interfaces;
+using WebApplication1.Infra.Repositories;
+using WebApplication1.Services.Interfaces;
+using WebApplication1.Services.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("MemotecaConnection");
+
+builder.Services.AddScoped<IDbConnection>(provider =>
+{
+    SqlConnection connection = new SqlConnection(connectionString);
+    connection.Open();
+    return connection;
+});
+
+// Configuração do AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Configuração dos serviços e repositórios
+builder.Services.AddScoped<IAutorService, AutorService>();
+builder.Services.AddScoped<IAutorRepository, AutorRepository>();
+
+// Configuração do controller com suporte a camelCase e case-insensitive no JSON
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure o pipeline HTTP
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
