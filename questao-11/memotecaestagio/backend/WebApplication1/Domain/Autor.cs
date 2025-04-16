99cbf1d60272ae17f1041bbf183366816e177ae5
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-16 14:05:22 -0300

    feat: criar a entidade autor e seu dto de criacao

---
+++ questao-11/memotecaestagio/backend/WebApplication1/Domain/Autor.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApplication1.Domain
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string Pensamento { get; set; }
+        public string AutorNome { get; set; }
+        public int Modelo { get; set; }
+    }
+}
---
+++ questao-11/memotecaestagio/backend/WebApplication1/Dto/CreateAutorDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Dto
+{
+    public class CreateAutorDto
+    {
+        [Required(ErrorMessage = "O campo pensamento é obrigatório.")]
+        [MaxLength(300, ErrorMessage = "O campo não pode ultrapassar 300 caracteres.")]
+        public string Pensamento { get; set; }
+
+        [Required(ErrorMessage = "O campo AutorNome é obrigatório.")]
+        [MaxLength(50, ErrorMessage = "O campo não pode ultrapassar 50 caracteres.")]
+        public string AutorNome { get; set; }
+        [Required(ErrorMessage = "O campo Modelo é obrigatório.")]
+        public int Modelo { get; set; }
+    }
+}
