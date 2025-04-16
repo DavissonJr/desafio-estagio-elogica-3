085228aba90bdb954d5194dee88ecfc31b940de3
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-16 12:35:22 -0300

    feat: ajustar a logica para reutilizar o componente

--- questao-11/memotecaestagio/src/app/shared/components/cards/cards.component.ts
+++ questao-11/memotecaestagio/src/app/shared/components/cards/cards.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './cards.component.css'
 })
 export class CardsComponent {
-
+  @Input() modo: 'mural' | 'formulario' = 'mural';
 }
