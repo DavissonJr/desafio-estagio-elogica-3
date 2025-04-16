a4e201e029af66e12ec24f85e3acbd00d476ad09
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-16 15:47:34 -0300

    feat: impletar o roteamento

--- questao-11/memotecaestagio/frontend/src/app/pages/mural/mural.component.ts
+++ questao-11/memotecaestagio/frontend/src/app/pages/mural/mural.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MuralService } from '../../shared/services/mural/mural.service';
 import { CardsComponent } from '../../shared/components/cards/cards.component';
 import { NgIf, NgFor } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-mural',
   standalone: true,
-  imports: [CardsComponent],
+  imports: [CardsComponent, RouterModule],
   templateUrl: './mural.component.html',
   styleUrl: './mural.component.css'
 })
