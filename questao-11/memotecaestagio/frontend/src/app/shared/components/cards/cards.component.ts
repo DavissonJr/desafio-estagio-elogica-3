9513e989540484332cf0b744b129c8df6a48124b
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-17 14:11:46 -0300

    feat: adicao do modal de delacao junto a funcao de deletar apos clique no modal

--- questao-11/memotecaestagio/frontend/src/app/shared/components/cards/cards.component.ts
+++ questao-11/memotecaestagio/frontend/src/app/shared/components/cards/cards.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { ModalService } from "../../services/modal/modal.service";
 
 @Component({
   selector: 'app-cards',
@@ -16,8 +17,10 @@ export class CardsComponent {
   @Input() modelo: string = 'modelo1';
   @Input() sombra: string = '';
   @Input() id?: number;
+  
+  constructor(private modalService: ModalService) { }
 
-  // enviar do fi p pai passando o numero que vai ser o id que vai deletar 
+  // enviar do fi p pai passando o numero que vai ser o id que vai deletar
   @Output() excluir = new EventEmitter<number>();
 
   get sombraClasse() {
@@ -28,5 +31,9 @@ export class CardsComponent {
     if (this.id !== undefined) {
       this.excluir.emit(this.id);
     }
+  }
+
+  showExcludeModal() {
+    this.modalService.showExcludeModal(() => this.onExcluir());
   }
 }
