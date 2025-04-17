1c753d411e784716e7f2e9db076d9e82294167ac
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-17 13:14:09 -0300

    feat: adicionar paginacao

--- questao-11/memotecaestagio/frontend/src/app/pages/mural/mural.component.ts
+++ questao-11/memotecaestagio/frontend/src/app/pages/mural/mural.component.ts
@@ -11,17 +11,48 @@ import { RouterModule } from '@angular/router';
   styleUrl: './mural.component.css'
 })
 export class MuralComponent implements OnInit {
+  paginaAtual = 0;
+  itensPorPagina = 6;
+
   constructor(public muralService: MuralService) {}
 
   async ngOnInit() {
     await this.muralService.carregarPensamentos();
   }
 
-  pensamentos() {
-    return this.muralService.pensamentos;
+  pensamentosPaginados() {
+    const inicio = this.paginaAtual * this.itensPorPagina;
+    const fim = inicio + this.itensPorPagina;
+    return this.muralService.pensamentos.slice(inicio, fim);
+  }
+
+  proximaPagina() {
+    if (this.temProximaPagina()) {
+      this.paginaAtual++;
+    }
+  }
+
+  paginaAnterior() {
+    if (this.temPaginaAnterior()) {
+      this.paginaAtual--;
+    }
+  }
+
+  temProximaPagina() {
+    return (this.paginaAtual + 1) * this.itensPorPagina < this.muralService.pensamentos.length;
+  }
+
+  temPaginaAnterior() {
+    return this.paginaAtual > 0;
   }
 
   async excluirPensamento(id: number) {
     await this.muralService.excluirPensamento(id);
+
+    // Se você estiver na última página e excluir tudo dela, volta para a anterior
+    const totalPaginas = Math.ceil(this.muralService.pensamentos.length / this.itensPorPagina);
+    if (this.paginaAtual >= totalPaginas) {
+      this.paginaAtual = Math.max(0, totalPaginas - 1);
+    }
   }
 }
