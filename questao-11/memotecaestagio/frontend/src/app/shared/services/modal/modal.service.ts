2ee161c4214dd9badb015fd3b9de6fcd3adccdc8
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-17 14:11:12 -0300

    feat: criar modais de exclusao e insercao

--- questao-11/memotecaestagio/frontend/src/app/shared/services/modal/modal.service.ts
+++ questao-11/memotecaestagio/frontend/src/app/shared/services/modal/modal.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,42 @@ import { Injectable } from '@angular/core';
 export class ModalService {
 
   constructor() { }
+
+  showSuccessModal(){
+    Swal.fire({
+      title: "Parabéns, filósofo!",
+      text: "Seu pensamento foi salvo com sucesso!",
+      icon: "success"
+    });
+  }
+
+  showErrorModal(){
+    Swal.fire({
+      title: "Deu erro, filósofo!",
+      text: "Verifique se você preencheu todos os dados corretamente!",
+      icon: "error"
+    });
+  }
+    // passar callback function
+  showExcludeModal(aoConfirmar: () => void) {
+    Swal.fire({
+      title: "Você tem certeza?",
+      text: "Você não poderá reverter essa ação!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sim, deletar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deletado!",
+          text: "O pensamento foi removido.",
+          icon: "success"
+        });
+
+        aoConfirmar();
+      }
+    });
+  }
 }
