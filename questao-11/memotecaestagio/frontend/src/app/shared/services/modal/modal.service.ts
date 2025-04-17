8d8e467914a0524d4c01db8a1073972602d41a9d
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-17 14:29:10 -0300

    feat: alterar texto do modal

--- questao-11/memotecaestagio/frontend/src/app/shared/services/modal/modal.service.ts
+++ questao-11/memotecaestagio/frontend/src/app/shared/services/modal/modal.service.ts
@@ -2,43 +2,42 @@ import { Injectable } from '@angular/core';
 import Swal from 'sweetalert2';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ModalService {
+  constructor() {}
 
-  constructor() { }
-
-  showSuccessModal(){
+  showSuccessModal() {
     Swal.fire({
-      title: "Parabéns, filósofo!",
-      text: "Seu pensamento foi salvo com sucesso!",
-      icon: "success"
+      title: 'Parabéns, pensador!',
+      text: 'Seu pensamento foi salvo com sucesso!',
+      icon: 'success',
     });
   }
 
-  showErrorModal(){
+  showErrorModal() {
     Swal.fire({
-      title: "Deu erro, filósofo!",
-      text: "Verifique se você preencheu todos os dados corretamente!",
-      icon: "error"
+      title: 'Deu erro, pensador!',
+      text: 'Verifique se você preencheu todos os dados corretamente!',
+      icon: 'error',
     });
   }
-    // passar callback function
+  // passar callback function
   showExcludeModal(aoConfirmar: () => void) {
     Swal.fire({
-      title: "Você tem certeza?",
-      text: "Você não poderá reverter essa ação!",
-      icon: "warning",
+      title: 'Você tem certeza?',
+      text: 'Você não poderá reverter essa ação!',
+      icon: 'warning',
       showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Sim, deletar"
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sim, deletar',
     }).then((result) => {
       if (result.isConfirmed) {
         Swal.fire({
-          title: "Deletado!",
-          text: "O pensamento foi removido.",
-          icon: "success"
+          title: 'Deletado!',
+          text: 'O pensamento foi removido.',
+          icon: 'success',
         });
 
         aoConfirmar();
